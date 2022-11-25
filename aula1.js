d7ccaac3a83f9f7514a14d3d5cf05ded45377c0e
vitoriabarbosa13 <barbosavitoria915@gmail.com>
2022-11-25 12:42:26 +0000

    placar proximo jogo

--- aula1.js
+++ aula1.js
@@ -2,7 +2,7 @@ var http = require("http");
 var bratz = require("./biblioteca");
 http.createServer(function (req,res){
     res.writeHead(200, { "Content-Type": "text/html"});
-    res.write("<h2>Brasil 2 x 0 Servia </h2> <br>");
+    res.write("<h2>Brasil 2 x 1 Servia </h2> <br>");
     res.write("<p style=color:pink>" +Date().substring(16,24) + "<br>");
     res.write(bratz.barbie() + "</p> <br>")
     res.end("Bom diaaa");
