de9297510b94718b5a5490cf12008df58243c3b5
unknown <barbosavitoria915@gmail.com>
2022-12-08 10:41:15 -0300

    aula4exemplo2 - retornando com as variaveis e placar do próximo jogo

---
+++ parse.js
@@ -0,0 +1,11 @@
+var url = require("url");
+var adr = "http://localhost:8080/index.html?ano=2017&mes=dezembro";
+var q = url.parse(adr, true);
+
+console.log(q.host);
+console.log(q.pathname);
+console.log(q.search);
+console.log("Brasil 2 x 1 Croácia")
+
+var qdata = q.query;
+console.log(qdata.mes);
