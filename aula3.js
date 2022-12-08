69af5495bbd4a8fdb80c2299b11dd778b65c802b
unknown <barbosavitoria915@gmail.com>
2022-12-08 08:31:25 -0300

    aula3node

---
+++ aula3.js
@@ -0,0 +1,11 @@
+var http = require("http");
+var url = require("url");
+
+http.createServer(function(req,res){
+    res.writeHead(200, { "Content-Type": "text/html"});
+    res.write(req.url);
+    var q = url.parse(req.url, true).query;
+    var txt = "<br>dia:" +q.dia+ "<br> mes:" +q.mes+ "<br> ano:" +q.ano;
+    res.end(txt);
+
+}).listen(80);
